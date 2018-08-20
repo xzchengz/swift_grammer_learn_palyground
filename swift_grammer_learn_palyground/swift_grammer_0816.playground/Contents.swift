3ee6bb2cb7a940c703b04894d13e127c7407d2be
chengz <zhangcheng1@panda.tv>
2018-08-20 18:50:45 +0800

    练习代码

--- swift_grammer_learn_palyground/swift_grammer_0816.playground/Contents.swift
+++ swift_grammer_learn_palyground/swift_grammer_0816.playground/Contents.swift
@@ -138,23 +138,503 @@ import UIKit
 //
 //print(total)
 //
+//
+//
+//// 函数和闭包
+//func greet(person: String, day: String) -> String {
+//    return "Hello \(person), today is \(day)."
+//}
+//
+//greet(person: "John", day: "Wednesday")
+//
+//func greet1(_ person: String, on day: String) -> String {
+//    return "Hello \(person), today is \(day)."
+//}
+//
+//greet1("Jack", on: "Wednesday")
+//
+//
+//func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+//    var min = scores[0]
+//    var max = scores[0]
+//    var sum = 0
+//
+//    for score in scores {
+//        if score > max {
+//            max = score
+//        } else if score < min {
+//            min = score
+//        }
+//
+//        sum += score
+//    }
+//
+//    return (min, max, sum)
+//}
+//
+////使用元组来让一个函数返回多个值。该元组的元素可以用名称或数字来表示。
+//let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+//print(statistics.sum)
+//print(statistics.1)
+//
+//
+////函数可以带有可变个数的参数，这些参数在函数内表现为数组的形式
+//func sumOf(numbers:Int...) -> Int {
+//    var sum = 0
+//    for number in numbers {
+//        sum += number
+//    }
+//    return sum
+//}
+//
+//sumOf()
+//
+//sumOf(numbers: 42, 597, 12)
+//
+//
+//func returnFifteen() -> Int {
+//    var y = 10
+//    func add() {
+//        y += 5
+//    }
+//
+//    add()
+//    return y
+//}
+//
+//returnFifteen()
+//
+//
+////函数是第一等类型，这意味着函数可以作为另一个函数的返回值。
+//func makeIncrementer() -> ((Int) -> Int) {
+//    func addOne(number: Int) -> Int {
+//        return 1 + number
+//    }
+//    return addOne
+//}
+//
+//var increment = makeIncrementer()
+//increment(7)
+//
+////函数也可以当做参数传入另一个函数。
+//func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+//    for item in list {
+//        if condition(item) {
+//            return true
+//        }
+//    }
+//    return false
+//}
+//
+//func lessThanTen(number: Int) -> Bool {
+//    return number < 10
+//}
+//
+//var numbers = [20, 19, 7, 12]
+//hasAnyMatches(list: numbers, condition: lessThanTen)
+//
+//// todo 未看懂
+//
+//numbers.map({
+//    (number: Int) -> Int in
+//    let result = 3 * number
+//    return result
+//})
+//
+//
+//let mappedNumbers = numbers.map({ number in 3 * number })
+////let mappedNumbers = numbers.map({number in 1 * number})
+//
+//
+//print(mappedNumbers)
+//
+//
+//let sortedNumbers = numbers.sorted { $0 > $1}
+//print(sortedNumbers)
 
 
-// 函数和闭包
-func greet(person: String, day: String) -> String {
-    return "Hello \(person), today is \(day)."
+//对象和类
+
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
 }
 
-greet(person: "John", day: "Wednesday")
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription()
 
-func greet1(_ person: String, on day: String) -> String {
-    return "Hello \(person), today is \(day)."
+// ------------
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
 }
 
-greet1("Jack", on: "Wednesday")
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)"
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "my test square")
+test.area()
+test.simpleDescription()
 
 
-func calculateStatistics(scores: [])
+// getter setter
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
+
+
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+    
+}
+
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larget square")
+print(triangleAndSquare.triangle.sideLength)
+
+
+let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+let sideLength = optionalSquare?.sideLength
+
+
+
+// 枚举和结构体
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.ace
+let aceRawValue = ace.rawValue
+
+
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+}
+
+enum Suit {
+    case spades, hearts, diamonds, clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+}
+
+
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+
+enum ServerResponse {
+    case result(String, String)
+    case failure(String)
+}
+
+let success = ServerResponse.result("6:00 am", "8：09 pm")
+let failure = ServerResponse.failure("Out of cheese.")
+
+switch success {
+case let .result(sunrise, sunset):
+    print("Sunrise is at \(sunrise) and sunset is at \(sunset)")
+case let .failure(message):
+    print("Failure... \(message)")
+}
+
+
+
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+}
+
+let threeOfSpades = Card(rank: .three, suit: .spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+
+
+
+
+
+// 协议和拓展
+
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += "Now 100% adjusted."
+    }
+}
+
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+
+
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    
+    mutating func adjust() {
+        self += 42
+    }
+}
+
+print(7.simpleDescription)
+
+
+let protocolValue: ExampleProtocol = a
+print(protocolValue.simpleDescription)
+//print(protocolValue.anotherProperty)
+
+
+
+
+
+// 错误处理
+
+enum PrinterError: Error {
+    case outOfPaper
+    case noToner
+    case onFire
+}
+
+
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "Never Has Toner" {
+        throw PrinterError.noToner
+    }
+    return "Job sent"
+}
+
+do {
+    let printerResponse = try send(job: 1040, toPrinter, "Bi Sheng")
+} catch {
+    print(error)
+}
+
+
+// ....
+// ...
+// ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 泛型
+
+func repeatItem<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
+    var result = [Item]()
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+
+repeatItem(repeating: "knock", numberOfTimes: 4)
+
+enum OptionalValue<Wapped> {
+    case None
+    case Some(Wapped)
+}
+
+var possibleInteger: OptionalValue<Int> = .None
+possibleInteger = .Some(100)
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool
+    where T.Iterator.Element: Equatable, T.Iterator.Element == U.Iterator.Element {
+        for lhsItem in lhs {
+            for rhsItem in rhs {
+                if lhsItem == rhsItem {
+                    return true
+                }
+            }
+        }
+        return false
+}
+
+anyCommonElements([1, 2, 3], [3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
